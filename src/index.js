899a226ea80f65fdcb27f19525ff630ec9bfaf87
wtetsu <tewatana@gmail.com>
2018-07-27 22:02:44 +0900

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,3 @@
+import _ from "lodash";
+
+console.log(_.map([1, 2, 3, 4, 5, 6, 7], a => a * a));
---
+++ webpack.config.js
@@ -0,0 +1,19 @@
+const HtmlPlugin = require("html-webpack-plugin");
+module.exports = {
+  mode: process.env.NODE_ENV || "development",
+  entry: ["./src/index.js"],
+  output: {
+    filename: "bundle.js",
+    path: __dirname + "/public"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: "babel-loader",
+        exclude: /node_modules/
+      }
+    ]
+  },
+  plugins: [new HtmlPlugin()]
+};
